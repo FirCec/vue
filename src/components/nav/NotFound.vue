d877251bf4d982898a9ecececce6d5b52a2305dd
Firmin Cecillon <firmin_cecillon@yahoo.fr>
2022-10-21 17:44:08 +0200

    redirection & notfound page

---
+++ src/components/nav/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>Page Not Found!</h2>
+    <div>
+        <h2>
+            Maybe view our <router-link to="/teams">Teams</router-link>
+        </h2>
+        
+    </div>
+    
+</template>
+<style>
+a{
+    text-decoration: none;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const routes = [
+    {
+        path: '/',
+        redirect: '/teams'
+    },
     {
         path: '/teams',
         component: TeamsList
@@ -18,6 +23,10 @@ const routes = [
         path: '/teams/:teamId',
         component: TeamMembers,
         props: true
+    },
+    {
+        path: '/:notFound(.*)',
+        component: NotFound,
     }
 ];
 const router = createRouter({
